d99d1f9f57b71c94ccd79b499f34f72b53c3fd89
Raphael Lagarde <raf.lgd@hotmail.fr>
2024-04-28 12:05:44 -0400

    init

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+
+
+st.set_page_config(layout='centered', page_icon='Logo2b.png', page_title='Datadella | Password Hasher')
+hashed_pwd = None
+
+_, c = st.columns([.2, .8])
+with c:
+    st.header(':hash: Hashing Password App')
+st.write('')
+
+with st.form('entry form'):
+    st.write('Entrez votre mot de passe ci-dessous')
+    pwd = st.text_input('')
+    st.write('')
+    _, c = st.columns([.7, .3])
+    with c:
+        if st.form_submit_button('Encrypter le mot de passe'):
+            hashed_pwd = stauth.Hasher([pwd]).generate()
+
+if hashed_pwd is not None:
+    st.caption('Mot de passe encrypté')
+    st.success(hashed_pwd[0])
